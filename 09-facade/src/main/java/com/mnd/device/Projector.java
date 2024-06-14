8ee5b7e28684303fd50bff486d601105d8a6b61a
wyz <isyz.wang@outlook.com>
2024-06-14 10:28:05 +0800

    外观模式

---
+++ 09-facade/src/main/java/com/mnd/device/Projector.java
@@ -0,0 +1,17 @@
+package com.mnd.device;
+
+/**
+ * @Author wyz
+ * @Date 2024/6/14 10:20
+ * @Description
+ */
+public class Projector {
+    
+    public void on() {
+        System.out.println("打开投影仪...");
+    }
+    
+    public void off() {
+        System.out.println("关闭投影仪...");
+    }
+}
