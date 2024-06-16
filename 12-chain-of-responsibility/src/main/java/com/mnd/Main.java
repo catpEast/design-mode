e8e54fc0aebf93cc6a7e6aaf4c0abb74fad4e1a1
wyz <isyz.wang@outlook.com>
2024-06-16 22:24:15 +0800

    责任链模式

--- 12-chain-of-responsibility/src/main/java/com/mnd/Main.java
+++ 12-chain-of-responsibility/src/main/java/com/mnd/Main.java
@@ -2,9 +2,9 @@ package com.mnd;
 
 public class Main {
     public static void main(String[] args) {
-        ErrorLogger errorLogger = new ErrorLogger(3);
-        FileLogger fileLogger = new FileLogger(2);
-        ConsoleLogger consoleLogger = new ConsoleLogger(1);
+        ErrorLogger errorLogger = new ErrorLogger(AbstractLogger.ERROR);
+        FileLogger fileLogger = new FileLogger(AbstractLogger.INFO);
+        ConsoleLogger consoleLogger = new ConsoleLogger(AbstractLogger.DEBUG);
         
         errorLogger.setNextLogger(fileLogger);
         fileLogger.setNextLogger(consoleLogger);
