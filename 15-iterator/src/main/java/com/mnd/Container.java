0ca07b017b3cc98ee96325884ad32d647081bec5
wyz <isyz.wang@outlook.com>
2024-06-18 20:50:55 +0800

    迭代器模式

---
+++ 15-iterator/src/main/java/com/mnd/Container.java
@@ -0,0 +1,6 @@
+package com.mnd;
+
+public interface Container {
+    
+    Iterator getIterator();
+}
---
+++ 15-iterator/src/main/java/com/mnd/NameRepository.java
@@ -0,0 +1,31 @@
+package com.mnd;
+
+public class NameRepository implements Container {
+    
+    private String[] names = {"张三", "李四", "王五", "赵六"};
+    
+    @Override
+    public Iterator getIterator() {
+        return new NameIterator();
+    }
+    
+    private class NameIterator implements Iterator {
+        int index;
+
+        @Override
+        public boolean hasNext() {
+            if (index < names.length) {
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public Object next() {
+            if (hasNext()) {
+                return names[index++];
+            }
+            return null;
+        }
+    }
+}
